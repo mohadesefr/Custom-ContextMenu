339c03f2ba5459617bfd59999e432afcfc4985bf
Mohadese <mohadesefarmahini@yahoo.com>
2023-11-21 16:14:06 +0330

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+﻿const ourMenuElem = document.getElementById('contextMenu');
+document.body.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+
+    if (ourMenuElem.style.display === 'none') {
+        ourMenuElem.style.left = event.pageX + 'px';
+        ourMenuElem.style.top = event.pageY + 'px';
+        ourMenuElem.style.display = 'block';
+
+    } else {
+        ourMenuElem.style.left = event.pageX + 'px';
+        ourMenuElem.style.top = event.pageY + 'px';
+    }
+
+});
+
+
+
+document.body.addEventListener('click', () => {
+    ourMenuElem.style.display = 'none';
+})
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.keyCode === 27) {
+        ourMenuElem.style.display = 'none';
+    }
+});
